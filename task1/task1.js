575863fc224bbe1fb4a160a21aa6c249717d36f2
ivanzakora <61563685+ivanzakora@users.noreply.github.com>
2020-03-04 17:51:30 +0300

    Add files via upload

---
+++ task1/task1.js
@@ -0,0 +1,38 @@
+// Task 1:
+// Sum of Minimums!
+// Given a 2D array of size m * n. Your task is to write a function to find the sum of minimum value in each row.
+// For Example:
+
+// [
+//   [1, 2, 3, 4, 5],       // minimum value of row is 1
+//   [5, 6, 7, 8, 9],       // minimum value of row is 5
+//   [20, 21, 34, 56, 100]  // minimum value of row is 20
+// ]
+// So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+// Note: You will be always given non-empty array containing Positive values.
+
+// Examples
+
+// sumMin([[1,2,3],[33,22,55],[16,42,345,79,1]])   //1 + 22 + 1 = 24 
+// sumMin([[11,2],[323,2,55]])   //2 + 2 = 4 
+// sumMin([[13,11,2],[3,23,23,55],[23,55,3,77],[39,7,89,94]])   //2 + 3 + 3 + 7 = 15 
+
+
+
+alert(sumMin([[1,2,3],[33,22,55],[16,42,345,79,1]]));
+
+
+function sumMin(arr){
+ var sum = 0;
+
+    for (var j = 0; j < arr.length; j++){
+    var min = arr[j][0];
+    for (var i = 0; i < arr[j].length; i++) {
+        if (min > arr[j][i]) min = arr[j][i];
+        }
+    console.log(min);
+    sum = sum + min;
+    }
+return sum;
+}  
